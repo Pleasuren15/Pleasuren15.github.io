af5937cdba9e7dc1b012ab8adbd5df8b9f7863b8
Pleasure Ndhlovu <Pleasuren15@gmail.com>
2026-02-07 09:46:18 +0200

    wip: navbar much better

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,29 @@
+
+import { BorderGradientButton } from './border-gradient.tsx'
+import { Separator } from './ui/separator.tsx';
+
 function Navbar() {
     return (
-        <div className="mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-                <div>
+                <div className="flex items-center gap-12">
+                    <h1 className="">
+                        ps.dev
+                    </h1>
+                    <Separator orientation="vertical" className="hidden md:block" />
                     <ul className="flex space-x-4">
-                        <li><a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a></li>
-                        <li><a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a></li>
-                        <li><a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a></li>
-                        <li><a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a></li>
+                        <li><a href="#" className=" px-3 py-2 font-medium">Home</a></li>
+                        <li><a href="#" className=" px-3 py-2 font-medium">About</a></li>
+                        <li><a href="#" className=" px-3 py-2 font-medium">Projects</a></li>
+                        <li><a href="#" className=" px-3 py-2 font-medium">Contact</a></li>
                     </ul>
                 </div>
                 <div>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Sign In
-                    </button>
+                    <BorderGradientButton duration={1800000}
+                        className="inline-flex h-11 items-center gap-2 rounded-lg bg-white 
+                            dark:bg-neutral-900 px-6 text-sm font-semibold text-neutral-900 dark:text-white">
+                        Change Log
+                    </BorderGradientButton>
                 </div>
             </div>
         </div>
