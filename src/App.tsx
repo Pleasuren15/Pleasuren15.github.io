b25af7da7a59db82f6858e669dbeb81cbebe34bf
Pleasure Ndhlovu <Pleasuren15@gmail.com>
2026-02-07 07:56:21 +0200

    wip: background added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Silk from './components/Silk'
 
 function App() {
   return (
-    <>
-      <h1>ps-portfolio</h1>
-    </>
+    <div className="app-root">
+      <div className="silk-bg">
+        <Silk speed={4} scale={1} color="#011598ff" noiseIntensity={5} rotation={0} />
+      </div>
+
+      <div className="app-content">
+        {/* Page content goes here; it will sit above the Silk background */}
+      </div>
+    </div>
   )
 }
 
