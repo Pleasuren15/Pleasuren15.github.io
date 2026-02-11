197bde4a2c45e3617e699c3e979a2b0280e599fa
Pleasure Ndhlovu <Pleasure.Ndhlovu+product@derivco.co.za>
2026-02-11 16:39:14 +0200

    wip: encrypted texts added

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import AnimatedBorderAvatar from './AnimatedBorderAvatar';
+import DecryptedText from './DecryptedText';
 
 const LandingPage: React.FC = () => {
   return (
     <div className="flex items-center justify-between w-full mt-25">
       {/* Left side - Text */}
       <div className="items-center">
-        <h5 className="text-2xl text-white">Hello, I Am SBU</h5>
-        <h3 className="text-8xl font-bold mt-2 mb-4">
-          SOFTWARE ENGINEER
+        {/* <h5 className="text-2xl text-white">Hello, I Am SBU</h5> */}
+        <h3 className="text-2xl font-bold mt-2 mb-4">
+          <DecryptedText
+            text="HI, I AM PLEASURE NDHLOVU"
+            animateOn="view"
+            revealDirection="start"
+            sequential
+            useOriginalCharsOnly={false}
+          />
         </h3>
-        <p>Delivering Software With Precision, Tested For Reliability, And Built To Scale.</p>
+        <p>
+          <DecryptedText
+            text="Delivering Software With Precision, Tested For Reliability, And Built To Scale."
+            animateOn="view"
+            revealDirection="start"
+            sequential
+            useOriginalCharsOnly={false}
+          />
+
+        </p>
+        <h3 className="text-5xl font-bold mt-2 mb-4">
+          <DecryptedText
+            text="A SOFTWARE ENGINEER"
+            animateOn="view"
+            revealDirection="start"
+            sequential
+            useOriginalCharsOnly={false}
+          />
+        </h3>
       </div>
 
       {/* Right side - Avatar */}
