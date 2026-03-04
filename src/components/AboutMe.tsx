723f8fc0b5dcfc6fe35dc6582c40847961bae730
Pleasure Ndhlovu <pleasuren15@gmail.com>
2026-03-04 16:41:43 +0200

    about me content center aligned

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const AboutMe: React.FC = () => {
     return (
-        <div className='ml-4 mr-4'>
-            <h3 className="text-3xl sm:text-6xl md:text-8xl lg:text-[10rem]"> 
+        <div className='text-center ml-4 mr-4'>
+            <h3 className="text-3xl sm:text-6xl md:text-8xl lg:text-[10rem] leading-tight whitespace-nowrap overflow-hidden font-bold"> 
                 About Me
             </h3>
-            <p>
+            <div className="w-[25%] h-1 bg-red-500 mb-3 mx-auto"></div>
+            <p className='text-center'>
                 I am a software engineer with a passion for building scalable and efficient applications. With experience in various programming languages and frameworks, I enjoy tackling complex problems and delivering high-quality solutions. I am always eager to learn new technologies and stay updated with industry trends to continuously improve my skills and contribute to impactful projects.
             </p>
         </div>
