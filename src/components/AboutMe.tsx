07b28bb2d9db0414eed25cb54d81d69f8c11f6f2
Pleasure Ndhlovu <pleasuren15@gmail.com>
2026-03-04 16:47:45 +0200

    more changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const AboutMe: React.FC = () => {
     return (
-        <div className='text-center ml-4 mr-4'>
+        <div className='text-center ml-4 mr-4 w-[85%]'>
             <h3 className="text-3xl sm:text-6xl md:text-8xl lg:text-[10rem] leading-tight whitespace-nowrap overflow-hidden font-bold"> 
                 About Me
             </h3>
             <div className="w-[25%] h-1 bg-red-500 mb-3 mx-auto"></div>
-            <p className='text-center w-[75%] mx-auto text-lg sm:text-xl md:text-2xl lg:text-4xl font-medium leading-relaxed'>
+            <p className='text-center mx-auto text-lg sm:text-xl md:text-2xl lg:text-4xl font-medium leading-relaxed'>
                 I am a software engineer with a passion for building scalable and efficient applications. With experience in various programming languages and frameworks, I enjoy tackling complex problems and delivering high-quality solutions. I am always eager to learn new technologies and stay updated with industry trends to continuously improve my skills and contribute to impactful projects.
             </p>
         </div>
